fcdfa8bfba50061f858c72cb485ab77273aee7da
shruti rathore <rathoreshruti269@gmail.com>
2025-01-28 23:52:14 +0530

    debugged some apis

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -48,7 +48,7 @@ export const authOptions: NextAuthOptions = {
       if (user) {
         token._id = user._id?.toString(); // Convert ObjectId to string
         token.isVerified = user.isVerified;
-        token.isAcceptingMessages = user.isAcceptingMessage;
+        token.isAcceptingMessages = user.isAcceptingMessages;
         token.username = user.username;
       }
       return token;
@@ -57,7 +57,7 @@ export const authOptions: NextAuthOptions = {
       if (token) {
         session.user._id = token._id;
         session.user.isVerified = token.isVerified;
-        session.user.isAcceptingMessage = token.isAcceptingMessage;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
         session.user.username = token.username;
       }
       return session;
--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,24 +1,22 @@
-import {resend} from "@/lib/resend";
+import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from "@/types/ApiResponse";
+import { ApiResponse } from '@/types/ApiResponse';
 
-export async function sendVerificationEmail (
-    email: string,
-    username : string,
-    verifyCode : string,
-
-) : Promise<ApiResponse>{
-    try {
-        await resend.emails.send({
-            from: 'rathoreshruti269@gmail.com',
-            to: email,
-            subject: 'Anonymous Caption Verification Code',
-            react: VerificationEmail({ username, otp: verifyCode }),
-          });
-        return {success : true, message: 'verification email sent successfully'}
-        
-    } catch (emailError) {
-        console.log("Error sending verification email", emailError)
-        return {success : false, message: 'failed to send verification email'}
-    }
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: 'dev@hiteshchoudhary.com',
+      to: email,
+      subject: 'Mystery Message Verification Code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: 'Verification email sent successfully.' };
+  } catch (emailError) {
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+  }
 }
